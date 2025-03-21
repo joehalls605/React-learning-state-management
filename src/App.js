eb5a0ed769c53a639500c26f27706b15343a0b3e
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-03-21 14:49:13 +0000

    Passing down props and rendering components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+
+
 function App(){
 
     const [books, setBooks] = useState([]);
 
-    const createBook = (event) => {
-            console.log("hi");
+    const createBook = (title) => {
+            const updatedBooks = [
+                ...books,
+                {
+                    id: Math.round(Math.random() * 9999),
+                    title
+                },
+            ];
+            setBooks(updatedBooks);
     };
 
-    return <div>
-        <BookCreate onCreate={createBook}/>
-    </div>
+    return (
+        <div className="app">
+            <BookList books={books}/>
+            <BookCreate onCreate={createBook}/>
+        </div>
+        )
 }
 
 export default App;
