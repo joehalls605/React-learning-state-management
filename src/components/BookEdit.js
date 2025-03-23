50e627394e4e441a75000e234ed751734971326d
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-03-23 09:35:16 +0000

    Book edit functionality

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,22 @@
-function BookEdit(){
-    return <div>BookEdit</div>
+import { useState} from 'react';
+
+function BookEdit({book}){
+    const [title, setTitle] = useState(book.title);
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(title);
+    }
+
+    return <form className="book-edit" onSubmit={handleSubmit}>
+        <label>{title}</label>
+        <input className="input" onChange={handleChange}/>
+        <button className="button is-primary">Save</button>
+    </form>
 }
 
 export default BookEdit;
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow( {book, onDelete}){
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    }
+
+    const handleDeleteClick = () => {
         onDelete(book.id);
     }
 
-    return <div className="book-show">
-        {book.title}
+    let content = <h3>{book.title}</h3>;
+    if(showEdit){
+        content = <BookEdit book={book}/>;
+    }
+
+    return (
+        <div className="book-show">
+            <div>{content}</div>
         <div className="actions">
-            <button className="delete" onClick={handleClick}>Delete</button>
+            <button className="edit" onClick={handleEditClick}>Edit</button>
+            <button className="delete" onClick={handleDeleteClick}>Delete</button>
         </div>
     </div>
+    )
 }
 
 export default BookShow;
