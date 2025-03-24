c4941abd397066191d7565c3d3a759b3750ca67d
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-03-24 10:06:14 +0000

    Refactor of the whole application with context use.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
-import axios from 'axios';
+import BooksContext from './context/books';
 
 function App(){
 
-    const [books, setBooks] = useState([]);
+    const { fetchBooks } = useContext(BooksContext);
 
-    const fetchBooks = async () => {
-        const response = await axios.get("http://localhost:3001/books");
 
-        setBooks(response.data);
-    }
 
     useEffect(() => {
         fetchBooks(); // this is always called during intial redner because of useEffect
     }, []); // empty array, called on first render, and never called again.
-    
-    const editBookById = async (id, newTitle) =>{
-        const response = axios.put(`http://localhost:3001/books/${id}`, {
-            title: newTitle
-        });
-        console.log(response);
 
-        const updatedBooks = books.map((book) => {
-            if(book.id === id){
-                return {...book, ...response.data} // updated book object from the API
-            }
-            return book // otherwise just return book as usual
-        })
-        setBooks(updatedBooks);
-    }
-
-    const deleteBookById = async (id) => {
-
-        await axios.delete(`http://localhost:3001/books/${id}`);
-
-        const updatedBooks = books.filter((book) =>{
-            return book.id !== id;
-        })
-        setBooks(updatedBooks);
-    }
-
-    const createBook = async (title) => {
-
-        const response = await axios.post("http://localhost:3001/books", {
-            title
-        });
-
-            const updatedBooks = [
-                ...books,
-                response.data
-            ];
-            setBooks(updatedBooks);
-    };
 
     return (
         <div className="app">
-            <BookList onEdit={editBookById} onDelete={deleteBookById} books={books}/>
-            <BookCreate onCreate={createBook}/>
+            <BookList/>
+            <BookCreate/>
         </div>
         )
 }
--- src/context/books.js
+++ src/context/books.js
@@ -1,18 +1,66 @@
 import { createContext, useState } from 'react';
+import axios from "axios";
 
 // Creating a custom context to manage and share state across components
 const BooksContext = createContext();
 
 // The Provider component will wrap parts of the app and provide context data
 function Provider({ children }) {
-    const [count, setCount] = useState(5); // Local state to track the count
 
-    // Data and functions to share with other components
+    const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3001/books");
+
+        setBooks(response.data);
+    }
+
+
+    const editBookById = async (id, newTitle) =>{
+        const response = axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+        console.log(response);
+
+        const updatedBooks = books.map((book) => {
+            if(book.id === id){
+                return {...book, ...response.data} // updated book object from the API
+            }
+            return book // otherwise just return book as usual
+        })
+        setBooks(updatedBooks);
+    }
+
+    const deleteBookById = async (id) => {
+
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
+        const updatedBooks = books.filter((book) =>{
+            return book.id !== id;
+        })
+        setBooks(updatedBooks);
+    }
+
+    const createBook = async (title) => {
+
+        const response = await axios.post("http://localhost:3001/books", {
+            title
+        });
+
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+        setBooks(updatedBooks);
+    };
+
+
     const valueToShare = {
-        count,
-        incrementCount: () => {
-            setCount(count + 1); // Function to increase the count
-        }
+        books,
+        deleteBookById,
+        editBookById,
+        createBook,
+        fetchBooks
     };
 
     // Combining the data and sharing ability using the Provider
